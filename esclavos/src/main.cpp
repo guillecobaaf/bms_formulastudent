4e3bd99cbf0358d3fbff611538d928b15724bb8c
guillecobaf <guillecoba@gmail.com>
2021-05-14 00:57:26 +0100

    cargando librerias en esclavos

--- esclavos/src/main.cpp
+++ esclavos/src/main.cpp
@@ -1,4 +1,13 @@
 #include <Arduino.h>
+#include <stdint.h>
+#include <SoftwareSerial.h>
+//#include <SPI.h>
+#include "LT_SPI.h"
+#include "UserInterface.h"
+#include "LTC68042.h"
+#include "Average.h"
+
+
 
 void setup() {
   // put your setup code here, to run once:
